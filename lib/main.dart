e70a0aaec3c1b3627bba6b8271aed7190c651de0
ihedwards <144269599+ihedwards@users.noreply.github.com>
2024-05-15 12:56:05 -0500

    Comment main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,22 +9,22 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget { //creating the app
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ella is trying her best', //lolll
+      title: 'Ella is trying her best', //lolll. i really am
       theme: ThemeData( //theme in terms of colors
-        primarySwatch: Colors.blueGrey,
+        primarySwatch: Colors.blueGrey, //da color
       ),
       home: const MyHomePage(title: 'MoneyWoman'), //title you see in top left corner
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatefulWidget { //home page/pages
   const MyHomePage({super.key, required this.title});
 
   final String title;
@@ -37,13 +37,13 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   late List<Widget> _pages;
 
-  List<Map<String, String>> tableData = [];
+  List<Map<String, String>> tableData = []; //create the table, initially with nada data
   int _currentIndex = 0;
 
   @override
   void initState() {
     super.initState();
-    _pages = [
+    _pages = [ //creates the tabs/different pages
       Expenses(updateTableData: updateTableData), //allows for the expenses to be a tab and update table
       const DataPage(), //creates data tab
       Income(updateTableData: updateTableData), //allows for the expenses to be a tab and update table
@@ -52,14 +52,14 @@ class _MyHomePageState extends State<MyHomePage> {
   }
   
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) { //whats in the app
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
-        backgroundColor: const Color.fromARGB(255, 200, 173, 207), //colors
+        backgroundColor: const Color.fromARGB(255, 200, 173, 207), //colors!!!!
       ),
       body: _pages[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
+      bottomNavigationBar: BottomNavigationBar( //whats in the navigation bar @ bottom and how state works
         currentIndex: _currentIndex,
         onTap: (index) {
           setState(() {
@@ -84,9 +84,9 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  void updateTableData(List<Map<String, String>> newData) {
+  void updateTableData(List<Map<String, String>> newData) { //update it all
     setState(() {
-      tableData = newData;
+      tableData = newData; //this data becomes that data
     });
   }
 }
